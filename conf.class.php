4f763ba012c6434e873fd8c095d37acae5a575a5
deadlyacid <deadlyacid@gmail.com>
2014-03-25 18:32:53 +0200

    configuration static class

--- conf.class.php
+++ conf.class.php
@@ -1,6 +1,49 @@
 <?php
+
+abstract class conf
+{
+    static private $protected = array(); //For DB / Passwords etc
+    static private $public = array(); //For all public strings such as meta stuff for site
+
+    public static function getProtected($key)
+    {
+        return isset(self::$protected[$key]) ? self::$protected[$key] : false;
+    }
+
+    public static function getPublic($key)
+    {
+        return isset(self::$public[$key]) ? self::$public[$key] : false;
+    }
+
+    public static function setProtected($key,$value)
+    {
+        self::$protected[$key] = $value;
+    }
+
+    public static function setPublic($key,$value)
+    {
+        self::$public[$key] = $value;
+    }
+
+    public function __get($key)
+    {//$this->key // returns public->key
+        return isset(self::$public[$key]) ? self::$public[$key] : false;
+    }
+
+    public function __isset($key)
+    {
+        return isset(self::$public[$key]);
+    }
+}
+
 // Define configuration
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "tim");
+
+conf::setProtected('KenyonIP_Matalon','192.168.1.12');
+conf::setProtected('KenyonIP_Tikva','192.168.1.13');
+
+
+conf::setProtected('REMOTE_IP','192.168.1.12' );
--- test.php
+++ test.php
@@ -1,4 +1,12 @@
 <?php
-
+/*
 $hours = '05:30';
 print_r(explode(':', $hours));
+*/
+function __autoload($classname) {
+    $filename = $classname .".class.php";
+    include_once($filename);
+}
+print_r($_SERVER, true);
+
+echo conf::getProtected('REMOTE_IP');
